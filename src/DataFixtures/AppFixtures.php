56131c8332b36f98402005fb4436e2a0c055d289
sebastien <naertsebastien@gmail.com>
2025-10-13 10:27:21 +0200

    Cours 3 - image entity

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,7 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
-use Cocur\Slugify\Slugify;
+use App\Entity\Image;
+// use Cocur\Slugify\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -16,15 +17,14 @@ class AppFixtures extends Fixture
         // $manager->persist($product);
 
         $faker = Factory::create("fr_FR");
-        $slugify = new Slugify();
 
-        for($i = 1; $i <= 30; $i++)
-        {
+        for ($i = 1; $i <= 30; $i++) {
             $ad = new Ad();
             $title = $faker->sentence();
-            $coverImage = "https://picsum.photos/id/".$i."/1000/350";
+            $coverImage = "https://picsum.photos/id/" . $i . "/1000/350";
             $introduction = $faker->paragraph(2);
-            $content = "<p>".join('</p><p>',$faker->paragraphs(5))."</p>";
+            $content = "<p>" . join('</p><p>', $faker->paragraphs(5)) . "</p>";
+
             $ad->setTitle($title)
                 ->setCoverImage($coverImage)
                 ->setIntroduction($introduction)
@@ -33,6 +33,16 @@ class AppFixtures extends Fixture
                 ->setRooms(rand(1, 5));
 
             $manager->persist($ad);
+
+            // Gestion des images de l'annonce (galerie)
+            for ($j = 1; $j <= rand(2, 5); $j++) {
+
+                $image = new Image();
+                $image->setAd($ad)
+                    ->setUrl("https://picsum.photos/id/" . $j . "/900")
+                    ->setCaption($faker->sentence());
+                $manager->persist($image);
+            }
         }
 
         $manager->flush();
