b20bbbaa14f1fe684069b3e4ea4874291c19dea1
sebastien <naertsebastien@gmail.com>
2025-10-13 14:16:46 +0200

    cours 3 - Affichage Ad v2

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
 use App\Repository\AdRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,15 +13,29 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AdController extends AbstractController
 {
     #[Route('/ads', name: 'ads_index')]
-    // Injection de dépendance : (AdRepository $repo)
     public function index(AdRepository $repo): Response
     {
-        // Appel au model
+        // appel au model
         $ads = $repo->findAll();
 
-        // Vue 
+        // vue
         return $this->render('ad/index.html.twig', [
             'ads' => $ads
+        ]);
+    }
+
+    /**
+     * Permet d'afficher la page de l'annonce choisie par l'utilisateur avec son slug
+     * @param Ad $ad
+     * @return Response
+     */
+    #[Route('/ads/{slug}', name: 'ads_show')]
+    public function show(
+        #[MapEntity(mapping: ['slug' => 'slug'])]
+        Ad $ad
+    ): Response {
+        return $this->render("ad/show.html.twig", [
+            "ad" => $ad
         ]);
     }
 }
